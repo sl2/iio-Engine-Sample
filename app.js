ca04ffc9174032046231441d195e514427c9aa95
sl2 <dimension.sf@gmail.com>
2015-02-01 02:30:07 +0900

    bug fix

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -27,7 +26,6 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
